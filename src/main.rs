7094d3f8152a26a2c7f18a68acab40aae0f74fa0
miguel <miguel@human.x.ai>
2019-07-16 12:07:48 -0400

    removing println

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,6 @@ mod week1;
 
 fn main() -> std::io::Result<()>{
     let g = week1::create_graph_from_file("SCC.txt")?;
-    //println!("{}",g.len());
-    //println!("{:?}",g);
     week1::dfs_loop(&g);
     Ok(())
-   //let mut g = HashMap::new();
-   //week1::dfs(&g, &0)
 }
