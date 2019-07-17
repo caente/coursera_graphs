e7d2bc9b1f89b0eba109ddff2a2a9652aff954c5
miguel <miguel@human.x.ai>
2019-07-17 10:09:56 -0400

    this works...

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 #![feature(drain_filter)]
 mod week1;
 
-fn main() -> std::io::Result<()>{
-    let g = week1::create_graph_from_file("graph-rev.txt")?;
-    //println!("{}",g.len());
-    println!("{:?}",g);
-    week1::dfs_loop(&g);
+fn main() -> std::io::Result<()> {
+    let g = week1::create_graph_from_file("graph.txt")?;
+    println!("graph created");
+    //println!("g:{:?}",g);
+    let g_rev = week1::reverse_graph(&g);
+    println!("reversed graph created");
+    //println!("g_rev:{:?}",g_rev);
+    let week1::Finished {
+        mut finishing_times,
+        ..
+    } = week1::dfs_loop(&g_rev);
+    println!("dfs finished");
+    let renamed_graph = week1::rename_graph(&g, &mut finishing_times);
+    println!("renamed graph");
+    //println!("renamed_graph:{:?}",renamed_graph);
+    let week1::Finished { mut sizes, .. } = week1::dfs_loop(&renamed_graph);
+    sizes.sort();
+    sizes.reverse();
+    sizes.truncate(5);
+    println!("Sizes: {:?}", sizes);
     Ok(())
-   //let mut g = HashMap::new();
-   //week1::dfs(&g, &0)
 }
--- src/week1.rs
+++ src/week1.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-pub type Graph = HashMap<u32, Vec<u32>>;
+pub type Graph = HashMap<usize, Vec<usize>>;
 
 pub fn create_graph_from_file(file_name: &str) -> Result<Graph, std::io::Error> {
     let file = File::open(file_name)?;
@@ -14,8 +14,8 @@ pub fn create_graph_from_file(file_name: &str) -> Result<Graph, std::io::Error> 
         match line {
             Ok(l) => {
                 let mut two_values = l.split(' ');
-                let node = two_values.next().unwrap().parse::<u32>().unwrap();
-                let adj = two_values.next().unwrap().parse::<u32>().unwrap();
+                let node = two_values.next().unwrap().parse::<usize>().unwrap();
+                let adj = two_values.next().unwrap().parse::<usize>().unwrap();
                 g.entry(node)
                     .and_modify(|adjs| adjs.push(adj))
                     .or_insert(vec![adj]);
@@ -26,42 +26,89 @@ pub fn create_graph_from_file(file_name: &str) -> Result<Graph, std::io::Error> 
     Ok(g)
 }
 
-pub fn dfs_loop(g: &Graph) -> Vec<u32> {
-    let mut keys: Vec<u32> = g.keys().fold(Vec::new(), |mut acc, &k| {
+pub fn reverse_graph(g: &Graph) -> Graph {
+    let mut rev: Graph = HashMap::new();
+    for (&node, _adj) in g {
+        let mut adj = _adj.clone();
+        while let Some(a) = adj.pop() {
+            rev.entry(a)
+                .and_modify(|adj| adj.push(node))
+                .or_insert(vec![node]);
+        }
+    }
+    rev
+}
+
+pub struct Finished {
+    pub finishing_times: Vec<usize>,
+    pub sizes: Vec<usize>,
+}
+
+pub fn rename_graph(g: &Graph, finishing_times: &mut Vec<usize>) -> Graph {
+    let mut new_labels: HashMap<usize, usize> = HashMap::new();
+    let mut renamed: Graph = HashMap::new();
+    let mut counter = 0;
+    finishing_times.reverse();
+    while let Some(n) = finishing_times.pop() {
+        counter = counter + 1;
+        new_labels.insert(n, counter);
+    }
+    for (&node, adj) in g {
+        let mut ad_cl = adj.clone();
+        let mut renamed_adj = Vec::new();
+        while let Some(a) = ad_cl.pop() {
+            renamed_adj.push(new_labels[&a]);
+        }
+        renamed.insert(new_labels[&node], renamed_adj);
+    }
+    renamed
+}
+
+pub fn dfs_loop(g: &Graph) -> Finished {
+    let mut keys: Vec<usize> = g.keys().fold(Vec::new(), |mut acc, &k| {
         acc.push(k);
         acc
     });
     keys.sort();
     //println!("KEYS:{:?}", keys);
-    let mut visited: HashSet<u32> = HashSet::new();
-    let mut finishing_order = Vec::new();
+    let mut visited: HashSet<usize> = HashSet::new();
+    let mut finishing_times = Vec::new();
+    let mut sizes = Vec::new();
     while let Some(n) = keys.pop() {
         if !visited.contains(&n) {
-            dfs(g, &n, &mut finishing_order, &mut visited);
+            let mut finishing_batch = dfs(g, &n, &mut visited);
+            sizes.push(finishing_batch.len());
+            finishing_times.append(&mut finishing_batch);
         }
     }
-    println!("Finished visiting {:?}", finishing_order);
-    let sizes = Vec::new();
-    sizes
+    //println!("Finished visiting {:?}", finishing_times);
+    //println!("Size {:?}", sizes);
+    Finished {
+        finishing_times,
+        sizes,
+    }
 }
-fn dfs(g: &Graph, n: &u32, finishing_order: &mut Vec<u32>, visited: &mut HashSet<u32>) {
+
+fn dfs(g: &Graph, n: &usize, visited: &mut HashSet<usize>) -> Vec<usize> {
     let mut to_visit = vec![*n];
+    let mut finishing_times = Vec::new();
     while let Some(next) = to_visit.pop() {
         visited.insert(next);
-        let mut current_adj: Vec<u32> = adjacents(g, next, visited);
+        let mut current_adj: Vec<usize> = adjacents(g, next, visited);
         match current_adj.pop() {
             Some(a) => {
                 to_visit.push(next);
                 to_visit.push(a);
             }
             None => {
-                finishing_order.push(next);
+                finishing_times.push(next);
             }
         }
     }
+    finishing_times
 }
 
-fn adjacents(g: &Graph, n: u32, visited: &mut HashSet<u32>) -> Vec<u32> {
+fn adjacents(g: &Graph, n: usize, visited: &mut HashSet<usize>) -> Vec<usize> {
     g.get(&n)
         .map(|adj| adj.clone())
         .unwrap_or(Vec::new())
